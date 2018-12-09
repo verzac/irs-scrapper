08ef3bee6c4b1ae0b721b7ee3cea6041df3e2d1a
verzac <benjamin.tanone@gmail.com>
2018-12-10 09:51:15 +1100

    bumped to 500 ms

--- irs.js
+++ irs.js
@@ -69,7 +69,7 @@ function extrapolate(csvRowArr) {
         var irsDocument = irsDom.window.document;
         // extractedLine is something like "Approximately 2 Miles"
         var extractedLine = irsDocument.querySelector('#main > div.body > div.content > div > table:nth-child(4) > tbody > tr:nth-child(8) > td:nth-child(2) > font');
-        if (extractedLine == null || !/Approximately\s+\d+\s+Miles/.test(extractedLine.textContent)) {
+        if (extractedLine == null || (!/Approximately\s+\d+\s+Miles/.test(extractedLine.textContent) && extractedLine.textContent != 'Same Town/City')) {
             extractedLine = irsDocument.querySelector('#main > div.body > div.content > div > table:nth-child(4) > tbody > tr:nth-child(5) > td:nth-child(2) > font');
         }
         if (extractedLine == null) {
@@ -89,6 +89,9 @@ function extrapolate(csvRowArr) {
         if (isNaN(Number(result))) {
             console.error(body);
             console.error(csvRowArr);
+            console.error(result);
+            if (extractedLine)
+                console.error(extractedLine.textContent);
             throw 'result is not a number!';
         }
         return result;
@@ -164,7 +167,7 @@ var csvStream = fastCsv().on("data", async (data) => {
     line += 1;
     if (data[0] != '') {
         var oldLine = line;
-        await sleep((line * 1000)); // 7 requests per 3 seconds aye
+        await sleep((line * 500)); // 7 requests per 3 seconds aye
         console.log('Extrapolating...', oldLine);
         // if (data[0] == '1773')
         // if (oldLine == 1825)
